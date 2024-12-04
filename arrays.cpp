f4c0ec825f525a9f185e97ec0db1b89486429c0c
cevolaniconnor <70539453+cevolaniconnor@users.noreply.github.com>
2024-12-04 14:41:19 -0800

    Developing basic understand of arrays. Declaring, counting, and printing.

--- arrays.cpp
+++ arrays.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main(){
 
-	
+	string cars[4] = {"Porsche", "BMW", "Audi", "Bentley"};
+
+	int length = sizeof(cars) / sizeof(cars[0]);
+
+	cout << length << endl;
+
+	for (string vechicle: cars){
+		cout << vechicle << endl;
+	}
+
 	return 0;
 }
